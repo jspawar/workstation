2e89dbaf006de937bfd86f4fde3cf37c451add45
Jaskanwal Pawar <mail@jaskanwal.dev>
2021-05-20 16:17:22 -0700

    Add gitconfig to dotfiles module

--- modules/dotfiles/install.sh
+++ modules/dotfiles/install.sh
@@ -6,5 +6,6 @@ echo "Linking various dotfiles..."
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # TODO: have it just loop over all directories here
-# Bash
-stow -d "${SCRIPT_DIR}" -t "${HOME}" bash
+for pkg in "${SCRIPT_DIR}"/*/; do
+    stow -d "${SCRIPT_DIR}" -t "${HOME}" "$(basename "${pkg}")"
+done
--- modules/dotfiles/verify.sh
+++ modules/dotfiles/verify.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-# Bash
+# bash
 test -f "${HOME}/.bashrc"
 test -f "${HOME}/.bash_profile"
 test -f "${HOME}/.inputrc"
 test -d "${HOME}/.config/bash"
+
+# git
+test -f "${HOME}/.gitconfig"
+test "$(git config --get alias.br)" = "branch"
