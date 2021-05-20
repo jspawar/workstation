86308992e988ca755c268ed87b2c7959b484ee7c
Jaskanwal <mail@jaskanwal.dev>
2021-05-19 17:41:57 -0700

    Brewfile module installation actually installs

--- modules/brewfile/install.sh
+++ modules/brewfile/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "Installing various utilities via homebrew..."
+brew bundle install --file="${SCRIPT_DIR}/Brewfile"
