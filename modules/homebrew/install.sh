8a35d794113311941d9adf646dca04a73697b640
Jaskanwal <mail@jaskanwal.dev>
2021-05-19 18:16:44 -0700

    Add module for tmux config
    - Also made minor tweak to add useful shopt's to homebrew installation script

--- modules/homebrew/install.sh
+++ modules/homebrew/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 echo "Installing homebrew..."
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
---
+++ modules/tmux/verify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# verify tmux is actually installed
+command -v tmux
+
+# verify config exists on filesystem
+test -d "${HOME}/.config/tmux"
+test -d "${HOME}/.tmux/plugins/tpm"
+
+# verify plugins have been installed (using random one as a canary)
+test -d "${HOME}/.config/tmux/plugins/tmux-yank"
