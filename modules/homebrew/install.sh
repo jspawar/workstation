c8adbafe4163d9500b5ff8a90737a250cb381222
Jaskanwal Pawar <jspawar@github.com>
2021-06-02 16:21:52 -0700

    Fix homebrew, brewfile and golang modules
    - Homebrew module apparently needs to use `HEAD` instead of `master`?
    - Brewfile module needed to actually specify tap which contains emacs
    - Golang module needs to depend on dotfiles modules otherwise path will not include golang
      binaries => failed verification
    - Added some things to install in brewfile too (e.g. docker, alacritty)

--- modules/homebrew/install.sh
+++ modules/homebrew/install.sh
@@ -2,4 +2,4 @@
 set -eu
 
 echo "Installing homebrew..."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
